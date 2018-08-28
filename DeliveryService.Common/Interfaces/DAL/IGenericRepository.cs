4cc142dc58defca0abf4a09470937eace8fbb34f
Pedro Silva <pedrolmcc@gmail.com>
2018-08-28 16:13:57 +0100

    added missing interface.

---
+++ DeliveryService.Common/Interfaces/DAL/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using DeliveryService.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryService.Common.Interfaces.DAL
+{
+    public interface IGenericRepository<DTO> where DTO: class
+    {
+        DTO Get(int dtoId);
+        IEnumerable<DTO> ListAll();
+        void Save(DTO dto);
+        void Delete(int dtoId);
+    }
+}
