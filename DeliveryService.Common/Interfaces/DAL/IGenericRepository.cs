0e15e4b44b410c7910f7173fbb6f2602237b014c
Pedro Rosas <pedro.rosas.mcc@gmail.com>
2018-08-28 19:59:11 +0100

    added missing file.

---
+++ DeliveryService.Common/Interfaces/DAL/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using DeliveryService.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryService.Common.Interfaces.DAL
+{
+    public interface IGenericRepository<DTO> where DTO : class
+    {
+        IEnumerable<DTO> ListAll();
+        DTO Get(int entityId);
+
+        void Save(DTO model);
+        void Delete(int entityId);
+    }
+}
